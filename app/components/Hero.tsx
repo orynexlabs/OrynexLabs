8eaa5747f06132986e42e59a827460bac8efe4ea
Demilade362 <ademolade362@gmail.com>
2025-10-17 20:18:02 +0000

    updated the font

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,9 +14,9 @@ const Hero = () => {
       {/* 🔹 Main content */}
       <div className="container-width pt-20 px-4 sm:px-6">
         <div className="max-w-4xl mx-auto text-center fade-in">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight mb-6">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight mb-6 font-poppins">
             Building the Future of{' '}
-            <span className="text-primary-hero">Decentralized Work</span>
+            <span className="text-primary-hero font-poppins">Decentralized Work</span>
           </h1>
 
           <p className="text-base sm:text-lg md:text-xl text-muted-foreground mb-8 max-w-2xl mx-auto">
--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export const metadata: Metadata = {
           "@type": "Person",
           "name": "Oluwademilade Abatan",
           "jobTitle": "Co-Founder & CEO",
-           "image": "https://orynexlabs.com/img/ceo.jpeg",
+          "image": "https://orynexlabs.com/img/ceo.jpeg",
           "sameAs": [
             "https://x.com/demilade1002",
             "https://www.linkedin.com/in/oluwademilade-abatan-8a5010352/"
@@ -129,9 +129,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet"/>
+
       </head>
       <body
         className={`antialiased`}
