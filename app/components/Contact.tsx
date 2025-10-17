a16928bc0d606f28ab5e3724352d08aa4a4b12b5
Demilade362 <ademolade362@gmail.com>
2025-10-17 18:25:52 +0000

    updated the team page

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -70,7 +70,7 @@ const Contact = () => {
               </div>
               <div>
                 <h3 className="font-semibold mb-1">Email</h3>
-                <p className="text-muted-foreground">orynex.help@gmail.com</p>
+                <p className="text-muted-foreground">contact@orynexlabs.com</p>
               </div>
             </div>
 
