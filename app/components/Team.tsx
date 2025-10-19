9ee127748c720b6011b12842377eecbbee8120ba
Demilade362 <ademolade362@gmail.com>
2025-10-19 21:10:15 +0000

    updated the team

--- app/components/Team.tsx
+++ app/components/Team.tsx
@@ -154,7 +154,7 @@ const TeamGrid: FC<TeamGridProps> = ({ members }) => (
 const Team: FC = () => {
   const technicalTeam: TeamMember[] = [
     {
-      name: "Demilade Abatan",
+      name: "Oluwademilade Abatan",
       role: "CEO & Co-Founder",
       img: "/img/ceo.jpeg",
       linkedin: "https://linkedin.com/in/oluwademilade",
