f0e14eadedc196dcb033122507bcc8c3cd0e2a93
David Ogbaki <creativeogbaki04@gmail.com>
2025-10-26 19:54:29 +0100

    updated SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   siteUrl: 'https://orynexlabs.com', // your live domain
   generateRobotsTxt: true,           // also creates robots.txt
+  // Custom robots rules tuned for Next.js so crawlers can fetch static assets
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: ['/', '/_next/static/', '/_next/image/'],
+        disallow: ['/private/', '/admin/', '/api/'],
+      },
+    ],
+    additionalSitemaps: ['https://orynexlabs.com/sitemap.xml'],
+  },
 };
